860a7dd5f81a9cc45350e05eca88916092c923d9
sebkuu23033 <sebkuu23033@gmail.com>
2025-03-31 21:15:37 +0200

    Add app/src/main/java/com/example/todoapp/MainActivity.kt 1743448537108

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.todoapp.navigation.TodoNavGraph
+import com.example.todoapp.ui.theme.TodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TodoNavGraph()
+                }
+            }
+        }
+    }
+}
